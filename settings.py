2be91a892b0060fa02d7c57055c0a28fb3c3c38c
Lakshmi Vyasarajan <lakshmivyas@MacLak.local>
2009-01-12 02:24:30 -0700

    Got the site working with hyde

--- settings.py
+++ settings.py
@@ -1,62 +1,77 @@
-import os, datetime
+import os
+
 ROOT_PATH = os.path.dirname(__file__)
 
-# setting up directory paths
-TEMPLATE_DIRS = (
-    os.path.join(ROOT_PATH,'templates'),
-    os.path.join(ROOT_PATH,'pages')
-)
-STATIC_DIR = os.path.join(ROOT_PATH,'static')
-DEPLOY_DIR = os.path.join(ROOT_PATH,'deploy')
-IMAGES_DIR = os.path.join(ROOT_PATH,'images')
-TMP_DIR = os.path.join(ROOT_PATH, 'aym_tmp_files')
-PAGES_DIR = os.path.join(ROOT_PATH, 'pages')
+#Directories
+SITE_TEMPLATE_DIR = os.path.join(ROOT_PATH, 'templates')
+CONTENT_DIR = os.path.join(ROOT_PATH, 'content')
+MEDIA_DIR = os.path.join(ROOT_PATH, 'media')
+DEPLOY_DIR = os.path.join(ROOT_PATH, 'deploy')
+TMP_DIR = os.path.join(ROOT_PATH, 'deploy_tmp')
+BACKUPS_DIR = os.path.join(ROOT_PATH, 'backups')
+
+
+# {folder : extension : (processors)}
+# The processors are run in the given order and are chained.
+# Only a lone * is supported for folders. Path shoud be specifed 
+# starting under the media folder. For example, if you have media/css under 
+# your site root,you should specify just css. If you have media/css/ie you 
+# should specify css/ie for the folder name. css/* is not supported (yet).
+
+# Extensions do not support wildcards.
+GENERATE_ABSOLUTE_FS_URLS = True
+
+MEDIA_PROCESSORS = {
+	'*':{
+		'.css':('hyde.processors.TemplateProcessor','hyde.processors.YUICompressor',),
+		'.ccss':('hyde.processors.TemplateProcessor','hyde.processors.CleverCSS', 'hyde.processors.YUICompressor',),
+		'.hss':('hyde.processors.TemplateProcessor','hyde.processors.HSS', 'hyde.processors.YUICompressor',),
+		'.js':('hyde.processors.YUICompressor',)
+	} 
+}
+
+CONTEXT = {
+	'groups':("Products", "Open Source", "Blog", "About"),
+	'badge_selectors': {"badge_new": "#javascript"},
+	'products': (
+		{'name': 'Shelved', 'disabled': True, 'image': "Shelved_128x128.png"},
+		{'name': 'Goalce', 'disabled': False, 'image': "what2do.png"},             
+		{'name': 'Unknown1', 'disabled':True, 'image': "blank.png"},                
+		{'name': 'Unknown2', 'disabled':True, 'image': "blank.png"}),   
+	'code_categories': (
+		{'name': 'Python', 'disabled': True},
+		{'name': 'Javascript', 'disabled': False},                       
+		{'name': 'Cocoa', 'disabled': True}
+	),
+	'all_links': {
+		"jQuery v1.2.6" : "",
+		"jQuery UI v1.6RC4" : "",
+		"MIT": "",
+		"GPL": ""
+	}
+	
+}
+
+#Processor Configuration
 
 # path for YUICompressor, or None if you don't
 # want to compress JS/CSS. Project homepage:
 # http://developer.yahoo.com/yui/compressor/
-#YUI_COMPRESSOR = "./yuicompressor-2.4.1.jar"
-YUI_COMPRESSOR = None # if you don't want to use YUI compressor
+YUI_COMPRESSOR = "../hyde/tools/yuicompressor-2.4.1.jar"
+#YUI_COMPRESSOR = None 
 
 # path for HSS, which is a preprocessor for CSS-like files (*.hss)
 # project page at http://ncannasse.fr/projects/hss
-HSS_PATH = "./hss-1.0-osx"
+HSS_PATH = "../hyde/tools/hss-1.0-osx"
 #HSS_PATH = None # if you don't want to use HSS
 
+#Django settings
 
-# Set true if you want to use CleverCSS with all .css files.
-# Must be installed via "sudo easy_install CleverCSS" before
-# usage.
-# USE_CLEVER_CSS = True
-USE_CLEVER_CSS = True
-CLEVER_CSS_EXT = ".ccss"
-
-# setting up some helpful values
-STATIC_URL_FORMAT = u"/static/%s"
-STATIC_THUMBNAIL_FORMAT = STATIC_URL_FORMAT % u"thumbnail/%s"
-STATIC_IMAGE_FORMAT = STATIC_URL_FORMAT % u"image/%s"
-THUMBNAIL_SIZE = (128,128)
-EMAIL = u"lakshmi.vyas@gmail.com"
-
-# creating default rendering context
-CONTEXT = {
-    'email':EMAIL,
-    'now':datetime.datetime.now(),
-    'badge_selectors': {"badge_new": "#javascript" },
-    'products': [{'name': 'Shelved', 'disabled': True, 'image': "Shelved_128x128.png"},
-                 {'name': 'Goalce', 'disabled': False, 'image': "what2do.png"}, 
-                 {'name': 'Unknown1', 'disabled':True, 'image': "blank.png"}, 
-                 {'name': 'Unknown2', 'disabled':True, 'image': "blank.png"}],
-    'code_categories': [{'name': 'Python', 'disabled': True},
-                        {'name': 'Javascript', 'disabled': False},
-                        {'name': 'Cocoa', 'disabled': True}]
-}
-
-PAGES_TO_RENDER = (
-    u"index.html",
-)
+TEMPLATE_DIRS = ( SITE_TEMPLATE_DIR, CONTENT_DIR, MEDIA_DIR, TMP_DIR )
 
 INSTALLED_APPS = (
-    'aym_tags',
+	'hyde',
     'django.contrib.webdesign'
 )
+
+
