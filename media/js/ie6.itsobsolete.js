3f91786d5a6eda1578b3cb3f2daf2f43c0001a17
Lakshmi Vyasarajan <lakshmivyas@MacLak.local>
2009-01-27 01:22:49 -0700

    Its Obsolete Domain name fix

--- media/js/ie6.itsobsolete.js
+++ media/js/ie6.itsobsolete.js
@@ -1,13 +1,8 @@
 if((navigator.userAgent.toLowerCase().indexOf('msie 6') != -1) &&
  (navigator.userAgent.toLowerCase().indexOf('msie 7') == -1)){
-     function beginsWith(full, part){
-         if (full.length < part.length) return false;
-         else
-             return full.substring(0, part.length) === part;
-    }
     if( !document.referrer ||
-        (!beginsWith(document.referrer, document.location.hostname) &&
-        !beginsWith(document.referrer, "http://ie6.itsobsolete.com"))){
+        (document.referrer.indexOf(document.location.hostname) === -1 &&
+        document.referrer.indexOf("itsobsolete") === -1)){
         document.location.href =
          "http://ie6.itsobsolete.com?title=Ringce&url=" +
          escape(document.location.href);    
